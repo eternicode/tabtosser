87014fb1a75e135666f746a952f8099ff0f7cd3e
Andrew Rowls <andrew@eternicode.com>
2012-07-01 02:09:55 -0400

    Minimum API that will work: two locations, one global pool.  Pools held in local memory for now.

--- server/api.py
+++ server/api.py
@@ -5,18 +5,29 @@ from flask.views import MethodView
 
 from . import app
 
+pools = {
+    '1': [],
+    '2': [],
+    '@pool': [],
+}
 
 class LocationAPI(MethodView):
-    def get(self, pubkey):
-        if pubkey is None:
-            return json.dumps([])
+    def get(self, id=None):
+        if id is None:
+            return json.dumps(pools.keys())
         else:
+            if id in pools:
+                return json.dumps(pools[id])
             return json.dumps(None)
 
+    def put(self, id):
+        if id in pools:
+            if 'add' in request.form:
+                pools[id].append(request.form['add'])
+            if 'remove' in request.form:
+                pools[id].remove(request.form['remove'])
+        return '', 204
 
 location_api = LocationAPI.as_view('location_api')
-app.add_url_rule('/locations/', defaults={'pubkey': None},
-                view_func=location_api, methods=['GET',])
-app.add_url_rule('/locations/', view_func=location_api, methods=['POST',])
-app.add_url_rule('/locations/<pubkey>', view_func=location_api,
-                methods=['GET', 'PUT', 'DELETE'])
+app.add_url_rule('/locations/', view_func=location_api, methods=['GET'])
+app.add_url_rule('/locations/<id>', view_func=location_api, methods=['GET', 'PUT'])
